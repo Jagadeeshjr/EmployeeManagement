8a1d1f1a60ba2531d29646ec9ef1f6c94fbf5153
jagadeesh <Jagadeesh Reddy@LAPTOP-2BED4MLN>
2023-10-01 18:12:11 +0530

    removed comments in program.cs

--- EmployeeManagement/Program.cs
+++ EmployeeManagement/Program.cs
@@ -16,8 +16,6 @@ namespace EmployeeManagement
         {
             var builder = WebApplication.CreateBuilder(args);
 
-            // Add services to the container.
-
             builder.Services.AddDbContext<EmployeeDBContext>(
                 options => options.UseSqlServer(builder.Configuration.GetConnectionString("EmployeeDB")));
 
@@ -52,7 +50,7 @@ namespace EmployeeManagement
  options.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter()));
 
             builder.Services.AddControllers().AddNewtonsoftJson();
-            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
 
@@ -66,7 +64,6 @@ namespace EmployeeManagement
 
             var app = builder.Build();
 
-            // Configure the HTTP request pipeline.
             if (app.Environment.IsDevelopment())
             {
                 app.UseSwagger();
